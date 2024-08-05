635c37bb285cbff38b577540789c31d60ff8d9d9
DarkWister <darkwistercrak17@gmail.com>
2024-08-05 17:25:20 -0400

    Prueba C QA

--- Gestion_Academica.Web/Controllers/ProfesorController.cs
+++ Gestion_Academica.Web/Controllers/ProfesorController.cs
@@ -10,7 +10,7 @@ namespace Gestion_Academica.Web.Controllers
         private readonly IProfesorRepository profesorRepository;
         public ProfesorController(IProfesorRepository profesorRepository) 
         {
-            // Una prueba b
+            // Una prueba c QA
             this.profesorRepository = profesorRepository;
         }
         // GET: ProfesorController
